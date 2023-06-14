3d47e664a7f58b827f2fc1aeb329fec10390e9b4
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-14 15:51:59 +0200

    add snack4.js (complete)

--- js/snack4.js
+++ js/snack4.js
@@ -1,2 +1,63 @@
 // controllo js
 console.log(`JS OK`);
+
+// funzione per il math random
+const getRandomNumbers = (min,max) => {
+   const random = Math.floor(Math.random() * max + 1 - min) + min
+   return random
+};
+
+// array di oggetti
+const teams = [
+    {
+        name : `lazio`,
+        fouls : 0,
+        points : 0
+    },
+    {
+        name : `roma`,
+        fouls : 0,
+        points : 0
+    },
+    {
+        name : `inter`,
+        fouls : 0,
+        points : 0
+    },
+    {
+        name : `milan`,
+        fouls : 0,
+        points : 0
+    },
+    {
+        name : `juve`,
+        fouls : 0,
+        points : 0
+    },
+    {
+        name : `napoli`,
+        fouls : 0,
+        points : 0
+    },
+];
+
+
+// metodo forEach
+for (const team of teams) {
+    team.fouls = getRandomNumbers(1,40);
+    team.points = getRandomNumbers(1,100)    
+};
+
+// metodo map
+const teamsFouls = teams.map((team) => {
+    return {
+        teamNames : team.name,
+        teamFouls : team.fouls
+    }
+});
+
+// stampa in console
+console.log(teams);
+
+console.log(teamsFouls);
+
